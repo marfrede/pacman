dadbfdb2366283a83392b1d4f24a549c85024f54
marfrede <marvin.frede@hs-osnabrueck.de>
2021-02-20 16:06:23 +0100

    remove comments

--- src/ShaderLightMapper.cpp
+++ src/ShaderLightMapper.cpp
@@ -57,7 +57,7 @@ ShaderLightMapper::~ShaderLightMapper()
 
 void ShaderLightMapper::addLight(BaseLight* Light)
 {
-    std::cout << "add light" << std::endl;
+    // std::cout << "add light" << std::endl;
 	assert(Light);
 
 	if (lights().size() >= MaxLightCount)
